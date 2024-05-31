c3d7b1333b461012364e72167e26df098e725b77
mahmud19114 <saad19114mahmud@gmail.com>
2024-05-31 11:36:36 -0400

    Initial commit with lab files

---
+++ ops445/lab2/lab2c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+name = sys.argv[1]
+
+age = int(sys.argv[2])
+
+# Print the first line
+print("Hi " + name + ", you are " + str(age) + " years old.")
---
+++ ops445/lab2/lab2e.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: [Mahmud Alam]
+# Author ID: [111292223]
+# Date Created: [2024/05/30]
+
+# Initialize the timer object with a value of 10
+timer = 10
+
+# While loop that repeats until timer equals 0
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+# Print the final message when the timer reaches 0
+print("blast off!")
